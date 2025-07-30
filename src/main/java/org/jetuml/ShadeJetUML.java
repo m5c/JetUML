eb0350bf05d589f87edc7603a3748a9d340ecf33
Maximilian Schiedermeier <schiedermeier.maximilian@uqam.ca>
2025-07-30 07:07:40 -0400

    Fixed Mac App icon

--- src/main/java/org/jetuml/ShadeJetUML.java
+++ src/main/java/org/jetuml/ShadeJetUML.java
@@ -1,2 +1,11 @@
-package org.jetuml;public class ShadeJetUML {
+package org.jetuml;
+
+/**
+ * Required for packaging to jar, see: https://stackoverflow.com/a/57691362 Just delegates call to
+ * actual main, which extends javafx.
+ */
+public class ShadeJetUML {
+  public static void main(String[] args) {
+    JetUML.main(args);
+  }
 }
